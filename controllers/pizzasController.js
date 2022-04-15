cce82fa4a3e16f876ae681712ff2780102693f40
wise-man-distracted <magewelle@live.com>
2022-04-15 18:44:54 -0300

    finished

--- controllers/pizzasController.js
+++ controllers/pizzasController.js
@@ -3,14 +3,35 @@ const pizzas = require('../database/pizzas.json')
 module.exports = {
     listar: (req, res) => {
         res.render('pizzas', {
-            pizzas: pizzas
+            pizzas: pizzas,
+            busca: ''
           });
     },
     mostrar: (req, res) => {
         let pizzaId = req.params.id;
-        let pizza = pizzas.find(p => p.id == pizzaId);
+        let idProxima
+        let idAnterior
+        let posicao = pizzas.findIndex(p => p.id == pizzaId)
+        let pizzaFind = pizzas.find(p => p.id == pizzaId);
+        let pizza = pizzas[posicao]
+        if(pizza.id == pizzas[pizzas.length -1].id ) {
+            idProxima = pizzas[0].id
+        } else {
+            idProxima = pizza.id + 1
+        }
+        if(pizza.id == pizzas[0].id) {
+            idAnterior = pizzas[pizzas.length -1].id
+        } else {
+            idAnterior = pizza.id - 1
+        }
+        console.log(idAnterior)
+        console.log(idProxima)
+        console.log(posicao)
+        console.log(pizza.id)
         res.render('pizza', {
-            pizza: pizza
+            pizza: pizzaFind,
+            idAnterior: idAnterior,
+            idProxima: idProxima
         })
     },
     buscar: (req, res) => {
